5d5b8f9f43c623739dce27f345aab7d1cfedcf01
Paulo Ancheta <paulo.ancheta@gmail.com>
2026-02-15 11:02:24 -0800

    gem updater

---
+++ lib/gem/update.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "update/version"
+
+module Gem
+  module Update
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "gem/update"
+
+require "minitest/autorun"
